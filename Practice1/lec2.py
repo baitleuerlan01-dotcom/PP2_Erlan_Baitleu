9639801e0e1d82781d1d2fc278db8e89cbb9f88e
baitleuerlan01-dotcom <baitleu.erlan01@gmail.com>
2026-01-28 10:41:40 +0500

    Add Practice1 Python basics examples

--- Practice1/lec2.py
+++
@@ -1,9 +0,0 @@
-where = input("Go left or right?")
-sum=1
-while where =="right":
-    where = input("Go left or right?")
-    sum+=1
-    if sum==2:
-        print (":(")
-        break
-print("You got out!")
